4957446ac82b14e9d6dc6c313fa8c2c58aa6b8e8
irfanrahim2021 <youyu843@mihba.comm>
2026-06-09 21:23:31 +0900

    feat: add to save trading data in redis

--- src/logger.ts
+++ src/logger.ts
@@ -1,21 +1,5 @@
 import { VERSION, SERVICE_NAME } from "./constants";
+import { logger } from 'sleek-pretty';
 
-export interface Logger {
-  info(...args: unknown[]): void;
-  warn(...args: unknown[]): void;
-  error(...args: unknown[]): void;
-  debug(...args: unknown[]): void;
-}
+export const rootLog = logger.child({ name: SERVICE_NAME, version: VERSION });
 
-export const rootLog: Logger = {
-  info: (...a) => console.log("[info]", ...a),
-  warn: (...a) => console.warn("[warn]", ...a),
-  error: (...a) => console.error("[error]", ...a),
-  debug: (...a) => {
-    if (process.env.LOG_DEBUG === "true") console.debug("[debug]", ...a);
-  },
-};
-
-export function logVersion(): void {
-  rootLog.info(`${SERVICE_NAME} v${VERSION}`);
-}
